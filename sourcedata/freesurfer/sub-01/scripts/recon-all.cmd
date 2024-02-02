e1335bd89f656e4202bed12d7e5f1a49414d42fa
jbwexler <jbwexler@stanford.edu>
2024-02-02 12:19:58 -0600

    unlock freesurfer

--- sourcedata/freesurfer/sub-01/scripts/recon-all.cmd
+++ sourcedata/freesurfer/sub-01/scripts/recon-all.cmd
@@ -1,1 +1,1 @@
-../../../../.git/annex/objects/xK/Mx/MD5E-s21925--b7de79daadec37f31dd91bc6d887fe89.cmd/MD5E-s21925--b7de79daadec37f31dd91bc6d887fe89.cmd
+/annex/objects/MD5E-s21925--b7de79daadec37f31dd91bc6d887fe89.cmd
